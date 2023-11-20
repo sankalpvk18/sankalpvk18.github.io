a867e8520c6168891b2be0df96338864f47a12e6
Sankalp <sankalpvk18@gmail.com>
2023-11-20 04:34:26 -0600

    project structure updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for the first time
         </a>
       </header>
     </div>
