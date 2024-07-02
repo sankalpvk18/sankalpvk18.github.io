4fc4d3ae7ab1df4f8b88f8009266597af370d148
Sankalp <sankalpvk18@gmail.com>
2024-07-02 03:13:49 -0500

    resume path changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
           ))}
           <li className="list-none hidden sm:flex flex-row gap-10">
             <a
-              href="/Resume%20-%20Sankalp%20Tripathi.pdf"
+              href="Resume - Sankalp Tripathi.pdf"
               download="Sankalp_Tripathi_Resume.pdf"
               className="flex text-white items-center font-bold shadow-md"
             >
