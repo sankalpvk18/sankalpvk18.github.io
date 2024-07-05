50b6905e5d14b2a6bd70c46a23e4860accf005e9
Sankalp <sankalpvk18@gmail.com>
2024-07-05 15:35:01 -0500

    banner issue on different resolution resolved

--- src/styles.js
+++ src/styles.js
@@ -4,13 +4,19 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
   
   heroHeadText: "font-black text-white lg:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] lg:leading-[80px] mt-2",
-  heroSubText: "text-[#dfd9ff] font-medium lg:text-[26px] sm:text-[22px] xs:text-[18px] text-[16px] lg:leading-[36px] mt-4",
+  heroSubText: "text-[#dfd9ff] font-medium lg:text-[26px] sm:text-[22px] xs:text-[18px] text-[16px] lg:leading-[36px]",
 
   sectionHeadText: "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText: "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 
   heroBackground: "bg-hero-custom",
-  techCard: ''
+  techCard: '',
+  // heroTextContainer: `
+  //   // relative z-10 pr-16
+  //   // @media (max-width: 1280px) { pr-24 }
+  //   // @media (max-width: 1024px) { pr-32 }
+  //   // @media (max-width: 768px) { pr-16 }
+  // `,
 };
 
 export { styles };
